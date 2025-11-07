ef908cd02ad1fbbbe764ace2fe76e46616f49e80
Quentin DI MEO <q.dimeo@outlook.com>
2025-11-07 11:48:59 +0100

    GG-xxx: initial commit r3fiber & zustand

--- client/app/canvas.tsx
+++ client/app/canvas.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
+import { forceSimulation } from "d3-force-3d";
 
 const CanvasComponent = () => {
+  const nodes = [{ id: "Alice" }, { id: "Bob" }, { id: "Carol" }];
+  const simulation = forceSimulation(nodes);
+  simulation.stop(); // Stop the simulation as we only need initial positions
+
   return <Canvas style={{ maxWidth: "10vw" }} />;
 };
 
